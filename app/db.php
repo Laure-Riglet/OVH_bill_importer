8d950333302f2c1fa9cc7a14bf61db96683238db
Laure-Riglet <laure.riglet@gmail.com>
2023-04-11 18:11:05 +0200

    wip: importing OVH invoices list

---
+++ app/db.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Utils;
+
+use PDO;
+
+// Database::getPDO()
+class Database
+{
+    /**
+     * PDO object representing the database connection
+     * @var PDO
+     */
+    private $dbh;
+
+    /**
+     * Static property (linked to the class) storing the unique object instance
+     * @var Database
+     */
+    private static $instance;
+
+    private function __construct()
+    {
+        $configData = parse_ini_file(__DIR__ . '/../config.ini');
+
+        try {
+            $this->dbh = new PDO(
+                "mysql:host={$configData['DB_HOST']};dbname={$configData['DB_NAME']};charset=utf8",
+                $configData['DB_USERNAME'],
+                $configData['DB_PASSWORD'],
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)
+            );
+        } catch (\Exception $exception) {
+            echo 'Connection to database failed:' . PHP_EOL;
+            echo $exception->getMessage() . PHP_EOL;
+            echo $exception->getTraceAsString() . PHP_EOL;
+            exit;
+        }
+    }
+
+    /**
+     * Returns the unique instance of the class
+     * @return PDO
+     */
+    public static function getPDO()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new Database();
+        }
+        return self::$instance->dbh;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# OVH INVOICE IMPORTER
+
+This is a simple PHP script to scrape new OVH invoices, store them locally and send alerts to an admin email address.
+
+## Requirements
+
+Install OVH SDK for PHP:
+
+```bash
+    composer require ovh/ovh
+```
+
+## Configuration
+
+Fill in the `config.ini` file with your OVH credentials and the admin email address.
